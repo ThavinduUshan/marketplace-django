e65d75234713a67164252708e5063c6735d7c54e
ThavinduUshan <thavinduushan@gmail.com>
2024-05-01 02:40:15 +0530

    products endpoints added

---
+++ store/serializers.py
@@ -0,0 +1,23 @@
+
+from decimal import Decimal
+from .models import Product, Collection
+from rest_framework import serializers
+
+
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description', 'slug', 'inventory',
+                  'unit_price', 'price_after_tax', 'collection']
+
+    price_after_tax = serializers.SerializerMethodField(
+        method_name='calculate_tax')
+
+    def calculate_tax(self, product: Product):
+        return product.unit_price * Decimal(1.1)
--- store/views.py
+++ store/views.py
@@ -1,5 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, get_list_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Product
+from tags.models import Tag
+from .serializers import ProductSerializer
 
 
-def index(request):
-    return render(request, 'store/index.html', {'name': 'Thavindu'})
+@api_view(['GET', 'POST'])
+def product_list(request):
+    if request.method == 'GET':
+        products = Product.objects.select_related('collection').all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET', 'POST', 'DELETE'])
+def product_detail(request, id):
+    product = get_object_or_404(Product, pk=id)
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data, )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'DELETE':
+        if product.orderitem_set.count() > 0:
+            return Response({'error': 'Product cannot be deleted as its associated with an order item.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
